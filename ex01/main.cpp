470241d3728759970555ac04f87964a237842e5a
Thomas Chobert <tchobert@f1br2s4.clusters.42paris.fr>
2025-05-19 15:49:08 +0200

    main tests ex01

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -11,116 +11,27 @@
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
+#include "ScavTrap.hpp"
 
-int main(void) {
-	ClapTrap a("Alpha");
-	ClapTrap b("Bravo");
+int main() {
+	std::cout << "=== Test Constructeurs ===" << std::endl;
+	ScavTrap s1;	// constructeur par défaut
+	ScavTrap s2("Guardian");	// constructeur avec nom
+	ScavTrap s3 = s2;	// constructeur de copie
 
-	a.setAttackDamages(5);
-	b.setAttackDamages(7);
+	std::cout << "\n=== Test Assignation ===" << std::endl;
+	s1 = s2;	// opérateur d'affectation
 
-	a.attack(b.getName());
-	b.takeDamage(a.getAttackDamages());
+	std::cout << "\n=== Test attack() ===" << std::endl;
+	s2.attack("Target1");
+	s2.attack("Target2");
 
-	b.attack(a.getName());
-	a.takeDamage(b.getAttackDamages());
+	std::cout << "\n=== Test guardGate() ===" << std::endl;
+	s2.guardGate();	// test du mode spécial
+	s2.guardGate(); // bis
+	s1.guardGate(); //s1
+	s3.guardGate(); //s3
 
-	a.beRepaired(3);
-	b.beRepaired(10);
-
-	a.takeDamage(100);  // tuer Alpha
-	a.attack(b.getName());  // doit bloquer
-	a.beRepaired(5);  // doit bloquer
-
-	ClapTrap copy;
-	copy = b;
-	copy.setName("CopyBravo");
-	copy.attack("Dummy");
-
-	return 0;
+	std::cout << "\n=== Fin des tests ===" << std::endl;
+	return (EXIT_SUCCESS);
 }
-
-// int	main(void) {
-// 	ClapTrap	clapOne;
-// 	ClapTrap	clapTwo("Number2");
-// 	ClapTrap	cloneTrap = clapTwo;
-// 	ClapTrap	copyTrap(clapOne);
-
-// 	std::cout << "\nINTRODUCING OUR GUESTS:" << std::endl;
-// 	std::cout << "clapOne is: " << clapOne.getName() << std::endl;
-// 	std::cout << "clapTwo is: " << clapTwo.getName() << std::endl;
-// 	std::cout << "cloneTrap is a clone of: " << cloneTrap.getName() << std::endl;
-// 	std::cout << "copyTrap is a copy of: " << copyTrap.getName() << std::endl;
-// 	std::cout << "\n!! LET'S FIGHT !!\n" << std::endl;
-
-// 	// Paramétrage des dégâts
-// 	clapOne.setAttackDamages(5);
-// 	clapTwo.setAttackDamages(4);
-// 	cloneTrap.setAttackDamages(3);
-// 	copyTrap.setAttackDamages(6);
-
-// 	// Round 1
-// 	clapOne.attack(copyTrap.getName());
-// 	copyTrap.takeDamage(clapOne.getAttackDamages());
-// 	std::cout << "copyTrap HP: " << copyTrap.getHitPoints() << "\n" << std::endl;
-
-// 	// Round 2
-// 	copyTrap.attack(clapOne.getName());
-// 	clapOne.takeDamage(copyTrap.getAttackDamages());
-// 	std::cout << "clapOne HP: " << clapOne.getHitPoints() << "\n" << std::endl;
-
-// 	// Round 3
-// 	cloneTrap.attack(copyTrap.getName());
-// 	copyTrap.takeDamage(cloneTrap.getAttackDamages());
-// 	std::cout << "copyTrap HP: " << copyTrap.getHitPoints() << "\n" << std::endl;
-
-// 	// Round 4 - Repair
-// 	copyTrap.beRepaired(5);
-// 	std::cout << "copyTrap repairs and now has " << copyTrap.getHitPoints() << " HP.\n" << std::endl;
-
-// 	// Round 5
-// 	clapTwo.attack(cloneTrap.getName());
-// 	cloneTrap.takeDamage(clapTwo.getAttackDamages());
-// 	std::cout << "cloneTrap HP: " << cloneTrap.getHitPoints() << "\n" << std::endl;
-
-// 	// Test de la mort
-// 	std::cout << "\n-- copyTrap is targeted for destruction --" << std::endl;
-// 	copyTrap.takeDamage(100);
-// 	copyTrap.attack(clapOne.getName());
-// 	copyTrap.beRepaired(5);
-// 	std::cout << "copyTrap HP: " << copyTrap.getHitPoints() << "\n" << std::endl;
-
-// 	// Test de l'énergie à 0
-// 	std::cout << "-- clapOne attacks until out of energy --" << std::endl;
-// 	for (int i = 0; i < 11; i++) {
-// 		clapOne.attack("dummy");
-// 	}
-// 	std::cout << "clapOne Energy: " << clapOne.getEnergyPoints() << "\n" << std::endl;
-
-// 	// Test des setters & getters
-// 	std::cout << "-- Setting stats for clapTwo --" << std::endl;
-// 	clapTwo.setHitPoints(30);
-// 	clapTwo.setEnergyPoints(5);
-// 	clapTwo.setAttackDamages(8);
-// 	std::cout << "clapTwo => HP: " << clapTwo.getHitPoints()
-// 				<< ", Energy: " << clapTwo.getEnergyPoints()
-// 				<< ", Damage: " << clapTwo.getAttackDamages() << "\n" << std::endl;
-
-// 	// Test affectation
-// 	std::cout << "-- Assignment test --" << std::endl;
-// 	ClapTrap another = clapTwo;
-// 	std::string newName = "AnotherClap";
-// 	another.setName(newName);
-// 	another.attack(clapOne.getName());
-
-// 	// Destructeur automatique
-// 	std::cout << "\n-- Temporary ClapTrap block --" << std::endl;
-// 	{
-// 		ClapTrap temp("TempTrap");
-// 		temp.attack("Nobody");
-// 	}
-// 	std::cout << "-- TempTrap should now be destroyed --\n" << std::endl;
-
-// 	std::cout << "=== END OF FIGHT ===" << std::endl;
-// 	return (EXIT_SUCCESS);
-// }
